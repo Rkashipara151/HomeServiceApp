975952bb0a5a1e4fe7259ed51578feec054075f3
hdhankecha <95201259+hdhankecha@users.noreply.github.com>
2022-10-07 11:56:51 +0530

    initial

--- app/src/main/java/com/example/homeserviceapp/LogIn.java
+++ app/src/main/java/com/example/homeserviceapp/LogIn.java
@@ -2,30 +2,49 @@ package com.example.homeserviceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class LogIn extends AppCompatActivity {
 
+    EditText EmailAddress,Password;
+    Button LogInBtn;
+    TextView Forgorpassword;
+    ImageView google;
+    String emailpatten = "[A_Za-z0-9._-]+@[a-z]+\\.+[a-z]+";
+    ProgressDialog progressDialog;
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
 
-        if (getSupportActionBar()!=null)
-        {
-            getSupportActionBar().hide();
-        }
+        EmailAddress = findViewById(R.id.EmailAddress);
+        Password = findViewById(R.id.Password);
+        LogInBtn = findViewById(R.id.LogInBtn);
+        Forgorpassword = findViewById(R.id.Forgorpassword);
+        google = findViewById(R.id.google);
 
-        Intent i = new Intent(LogIn.this,SignUp.class);
-
-        new Handler().postDelayed(new Runnable() {
+        Forgorpassword.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                startActivity(i);
+            public void onClick(View view) {
+                Intent fp = new Intent(LogIn.this,ForgotPassword.class);
+                startActivity(fp);
             }
-        },5000);
+        });
+
+
+
     }
 }
