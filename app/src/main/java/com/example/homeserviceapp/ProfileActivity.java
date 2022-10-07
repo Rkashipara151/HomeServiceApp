355a6651a71f2e806d93ad7068ee47f6fb18c471
hdhankecha <95201259+hdhankecha@users.noreply.github.com>
2022-10-07 22:59:44 +0530

    initial

--- app/src/main/java/com/example/homeserviceapp/ProfileActivity.java
+++ app/src/main/java/com/example/homeserviceapp/ProfileActivity.java
@@ -1,14 +1,39 @@
 package com.example.homeserviceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileActivity extends AppCompatActivity {
-
+    FirebaseAuth mAuth;
+    Button btnLogout;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-    }
+
+        btnLogout = findViewById(R.id.btnLogout);
+        mAuth = FirebaseAuth.getInstance();
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                Intent so = new Intent(ProfileActivity.this,LogIn.class);
+                startActivity(so);
+            }
+        });
+   }
+
 }
