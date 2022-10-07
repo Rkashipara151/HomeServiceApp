78ce63481a6a588864f2bd127bd01a9a047219ce
hdhankecha <95201259+hdhankecha@users.noreply.github.com>
2022-10-07 12:52:46 +0530

    initial

--- app/src/main/java/com/example/homeserviceapp/LogIn.java
+++ app/src/main/java/com/example/homeserviceapp/LogIn.java
@@ -1,5 +1,6 @@
 package com.example.homeserviceapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -12,7 +13,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -23,6 +28,7 @@ public class LogIn extends AppCompatActivity {
     TextView Forgorpassword;
     ImageView google;
     String emailpatten = "[A_Za-z0-9._-]+@[a-z]+\\.+[a-z]+";
+    TextView signu;
     ProgressDialog progressDialog;
     FirebaseAuth mAuth;
     FirebaseUser mUser;
@@ -39,6 +45,7 @@ public class LogIn extends AppCompatActivity {
         LogInBtn = findViewById(R.id.LogInBtn);
         Forgorpassword = findViewById(R.id.Forgorpassword);
         google = findViewById(R.id.google);
+        signu = findViewById(R.id.signu);
         mAuth = FirebaseAuth.getInstance();
         progressDialog = new ProgressDialog(LogIn.this);
 
@@ -51,7 +58,68 @@ public class LogIn extends AppCompatActivity {
             }
         });
 
+        signu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sp = new Intent(LogIn.this,SignUp.class);
+                startActivity(sp);
+            }
+        });
+
+        LogInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                perforauth();
+            }
+        });
 
 
+
+    }
+
+    private void perforauth() {
+        String Email = EmailAddress.getText().toString();
+        String password = Password.getText().toString();
+
+        if(!Email.matches(emailpatten))
+        {
+            EmailAddress.setError("Please Valid Email");
+        }
+        else if(password.isEmpty() || password.length()<6)
+        {
+            Password.setError("Please proper Password");
+        }
+
+        else
+        {
+            progressDialog.setMessage("Please Waiting....");
+            progressDialog.setTitle("Login");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+        }
+        mAuth.signInWithEmailAndPassword(Email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                {
+                    progressDialog.dismiss();
+                    Toast.makeText(LogIn.this,"Login Successful",Toast.LENGTH_SHORT).show();
+                    sendUserToNextActivvity();
+                }
+                else
+                {
+                    progressDialog.dismiss();
+                    Toast.makeText(LogIn.this,"Login Error"+task.getException(),Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            private void sendUserToNextActivvity() {
+                Intent Main = new Intent(LogIn.this, DashBoard.class);
+                startActivity(Main);
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/homeserviceapp/SignUp.java
+++ app/src/main/java/com/example/homeserviceapp/SignUp.java
@@ -1,31 +1,123 @@
 package com.example.homeserviceapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.w3c.dom.Text;
 
 public class SignUp extends AppCompatActivity {
 
+    EditText Fname,Lname,email,Pass,Cpass;
+    Button btnSignUp;
+    TextView SLogin;
+    String emailpatten = "[A_Za-z0-9._-]+@[a-z]+\\.+[a-z]+";
+    ProgressDialog progressbar;
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        if (getSupportActionBar()!=null)
-        {
-            getSupportActionBar().hide();
-        }
+        Fname = findViewById(R.id.Fname);
+        Lname = findViewById(R.id.Lname);
+        email = findViewById(R.id.email);
+        Pass = findViewById(R.id.Pass);
+        Cpass = findViewById(R.id.Cpass);
+        btnSignUp = findViewById(R.id.btnSignUp);
+        SLogin = findViewById(R.id.SLogin);
+        mAuth = FirebaseAuth.getInstance();
+        progressbar = new ProgressDialog(SignUp.this);
 
-
-        Intent i = new Intent(SignUp.this,ForgotPassword.class);
-
-        new Handler().postDelayed(new Runnable() {
+        SLogin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                startActivity(i);
+            public void onClick(View view) {
+                Intent Login = new Intent(SignUp.this,LogIn.class);
+                startActivity(Login);
             }
-        },4000);
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                perforauth();
+            }
+        });
+
+
+
+    }
+
+    private static void setOnClickListener(View.OnClickListener onClickListener) {
+    }
+
+    private void perforauth() {
+
+        String Email = email.getText().toString();
+        String Password = Pass.getText().toString();
+        String Cpassword = Cpass.getText().toString();
+
+        if(!Email.matches(emailpatten))
+        {
+            email.setError("Please Valid Email");
+        }
+        else if(Password.isEmpty() || Password.length()<6)
+        {
+            Pass.setError("Please proper Password");
+        }
+        else if(!Password.equals(Cpassword))
+        {
+            Cpass.setError("Please do not match Password");
+        }
+        else
+        {
+            progressbar.setMessage("Please Waiting....");
+            progressbar.setTitle("Registration");
+            progressbar.setCanceledOnTouchOutside(false);
+            progressbar.show();
+
+            mAuth.createUserWithEmailAndPassword(Email,Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful())
+                    {
+//                        progressbar.dismiss();
+                        Toast.makeText(SignUp.this,"Registation successful",Toast.LENGTH_SHORT).show();
+                        sendUserToNextActivity();
+                    }
+                    else
+                    {
+//                        progressbar.dismiss();
+                        Toast.makeText(SignUp.this,"Registration Error"+task.getException(),Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    private void sendUserToNextActivity() {
+
+        Intent Main = new Intent(SignUp.this,LogIn.class);
+        startActivity(Main);
     }
 }
