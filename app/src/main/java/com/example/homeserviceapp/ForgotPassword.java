33149c55287ef2459c073b61f7c53da02f0c9447
hdhankecha <95201259+hdhankecha@users.noreply.github.com>
2022-10-07 15:04:56 +0530

    initial

--- app/src/main/java/com/example/homeserviceapp/ForgotPassword.java
+++ app/src/main/java/com/example/homeserviceapp/ForgotPassword.java
@@ -1,31 +1,93 @@
 package com.example.homeserviceapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.w3c.dom.Text;
 
 public class ForgotPassword extends AppCompatActivity {
+
+    EditText Femail;
+    Button Resetpass;
+    TextView signup;
+    String emailpatten = "[A_Za-z0-9._-]+@[a-z]+\\.+[a-z]+";
+    ProgressDialog progrssbar;
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
 
-        if (getSupportActionBar()!=null)
-        {
-            getSupportActionBar().hide();
+        Femail = findViewById(R.id.Femail);
+        Resetpass = findViewById(R.id.Resetpass);
+        signup = findViewById(R.id.signup);
+        mAuth = FirebaseAuth.getInstance();
+        progrssbar = new ProgressDialog(ForgotPassword.this);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sp = new Intent(ForgotPassword.this,SignUp.class);
+                startActivity(sp);
+            }
+        });
+
+        Resetpass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                perforauth();
+            }
+        });
+    }
+
+    private void perforauth() {
+
+        String Email = Femail.getText().toString();
+        if (!Email.matches(emailpatten)) {
+            Femail.setError("Please Valid Email");
+        } else {
+            progrssbar.setMessage("Please Waiting....");
+            progrssbar.setTitle("Sending...");
+            progrssbar.setCanceledOnTouchOutside(false);
+            progrssbar.show();
         }
 
-
-        Intent i = new Intent(ForgotPassword.this,DashBoard.class);
-
-        new Handler().postDelayed(new Runnable() {
+        mAuth.sendPasswordResetEmail(Email).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
-            public void run() {
-                startActivity(i);
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful())
+                {
+                    Toast.makeText(ForgotPassword.this,"Email Sent in spam",Toast.LENGTH_SHORT).show();
+                    sendUserToNextActivity();
+                }
+                else
+                {
+                     Toast.makeText(ForgotPassword.this,"Error"+task.getException(),Toast.LENGTH_SHORT).show();
+                }
             }
-        },4000);
+        });
+    }
+
+    private void sendUserToNextActivity() {
+        Intent Main = new Intent(ForgotPassword.this, LogIn.class);
+        startActivity(Main);
+
     }
 }
--- app/src/main/java/com/example/homeserviceapp/LogIn.java
+++ app/src/main/java/com/example/homeserviceapp/LogIn.java
@@ -73,9 +73,6 @@ public class LogIn extends AppCompatActivity {
                 perforauth();
             }
         });
-
-
-
     }
 
     private void perforauth() {
